68a7fd4a8dc329853c3b029fd0f1b36fcaf4beeb
sohrabeghasemi <sohrabeghasemi@gmail.com>
2019-04-08 02:29:20 -0700

    19/1/1398

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.kagar_cyrus)
+class PersonelAdmin(admin.ModelAdmin):
+	list_display = ('l_name', 'f_name')
+	fields = (('f_name','l_name'), ('meliyat', 'vaziyet_kari'), 'vorud_date')
+
+
--- web/models.py
+++ web/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+
+class kagar_cyrus(models.Model):
+
+	MELIYAT_CHOICES = (
+		('IR', 'ایرانی'),
+		('AF', 'افغانی'),
+	)
+
+	VAZIYAT_KARI_CHOICES = (
+		('krg', 'کارگر'),
+		('ost', 'استادکار'),
+	)
+	f_name = models.CharField('نام', max_length=200)
+	l_name = models.CharField('نام خانوادگی', max_length=250)
+	meliyat = models.CharField('ملیت', max_length=2, choices=MELIYAT_CHOICES)
+	vaziyet_kari = models.CharField('وضعیت کاری', max_length=3, choices=VAZIYAT_KARI_CHOICES)
+	vorud_date = models.DateField('تاریخ ورود به پروژه')
+
